83fd51e9dbe920d3215b3158cde33a4ad5e7dd29
lucas <lucas-mira1@hotmail.com>
2021-02-16 14:38:50 -0300

    Backend project created

---
+++ backend/src/test/java/com/LucasMira/DsLearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasMira.DsLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
